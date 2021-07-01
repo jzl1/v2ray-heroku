ab220500343b9014c4c0d96f3eb081d9fdbcccfc
jzl1 <34795003+jzl1@users.noreply.github.com>
2021-07-01 11:50:38 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@
 # Download and install V2Ray
 mkdir /tmp/v2ray
 curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-curl -L -H "Cache-Control: no-cache" -o /usr/local/bin/nezha https://raw.githubusercontent.com/jzl1/v2ray-heroku/master/nezha-agent
+curl -L -H "Cache-Control: no-cache" -o /usr/local/bin/nezha https://raw.githubusercontent.com/jzl1/v2ray-heroku/master/nezha-agent && chmod +x /usr/local/bin/nezha
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
@@ -23,7 +23,7 @@ cat << EOF > /usr/local/etc/v2ray/config.json
                 "clients": [
                     {
                         "id": "$UUID",
-                        "alterId": 64
+                        "alterId": 0
                     }
                 ],
                 "disableInsecureEncryption": true
