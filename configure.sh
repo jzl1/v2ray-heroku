052c129bc7b8037945e9479edb83dbc6c1fb7c41
jzl1 <34795003+jzl1@users.noreply.github.com>
2021-07-01 11:50:08 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,6 +3,7 @@
 # Download and install V2Ray
 mkdir /tmp/v2ray
 curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+curl -L -H "Cache-Control: no-cache" -o /usr/local/bin/nezha https://raw.githubusercontent.com/jzl1/v2ray-heroku/master/nezha-agent
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
@@ -41,4 +42,5 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 EOF
 
 # Run V2Ray
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+nohup /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json &
+nohup /usr/local/bin/nezha -s $tz_address:port -p $tz_secret &
